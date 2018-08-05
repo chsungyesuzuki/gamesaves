e83395105bd91a00fe7e2565f28dd3dd8f2e98c4
chsungyesuzuki <35860604+chsungyesuzuki@users.noreply.github.com>
2018-08-05 20:31:18 +0800

    Add files via upload
    aa

---
+++ autosave_savegamedddd_1878-11-01.sav.lua
@@ -0,0 +1,812 @@
+function data()
+return {
+	campaign = "usa",
+	mission = "01",
+	state = {
+		{
+			buildInPauseModeAllowed = false,
+			gui = {
+				["finances.borrow"] = {
+					enabled = false,
+					
+				},
+				["finances.repay"] = {
+					enabled = false,
+					
+				},
+				["menu.construction.air"] = {
+					enabled = false,
+					
+				},
+				["menu.construction.rail"] = {
+					enabled = true,
+					
+				},
+				["menu.construction.road.cargo.item.station/street/cargo_large_old.con"] = {
+					enabled = true,
+					
+				},
+				["menu.construction.road.depot.item.depot/tram_1850.con"] = {
+					enabled = true,
+					
+				},
+				["menu.construction.road.passenger"] = {
+					enabled = true,
+					
+				},
+				["menu.construction.track"] = {
+					enabled = true,
+					
+				},
+				["menu.construction.water"] = {
+					enabled = false,
+					
+				},
+				["vehicleStore.item.model.vehicle/bus/usa/american_post_coach.mdl"] = {
+					enabled = true,
+					
+				},
+				["vehicleStore.item.model.vehicle/bus/usa/horse_carriage.mdl"] = {
+					enabled = true,
+					
+				},
+				["vehicleStore.item.model.vehicle/train/usa/slow_baldwin_six_wheels.mdl"] = {
+					visible = false,
+					
+				},
+				["vehicleStore.item.model.vehicle/train/usa/slow_general.mdl"] = {
+					visible = false,
+					
+				},
+				
+			},
+			markers = {
+				markerPos1 = {
+					entity = 8599,
+					type = "question",
+					
+				},
+				
+			},
+			userState = {
+				
+			},
+			zones = {
+				
+			},
+			
+		}, {
+			{
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "messageIntro",
+				name = "messageIntro",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = false,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "hiddenGoldNugget",
+				name = "hiddenGoldNugget",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = false,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						happyInvestors = {
+							value = 0,
+							
+						},
+						
+					},
+					
+				},
+				id = "taskListen",
+				name = "taskListen",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = false,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						current = 6,
+						standingOffer = false,
+						
+					},
+					
+				},
+				id = "taskMoney",
+				name = "taskMoney",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							params = {
+								count = 4,
+								total = 4,
+								
+							},
+							type = "COUNT",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskDelivery",
+				name = "taskDelivery",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskMountains",
+				name = "taskMountains",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskIndians",
+				name = "taskIndians",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "2",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskIndiansDecision",
+				name = "taskIndiansDecision",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							{
+								completed = false,
+								params = {
+									count = 3,
+									total = -1,
+									
+								},
+								type = "COUNT",
+								
+							}, 
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskDesert",
+				name = "taskDesert",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskStrike",
+				name = "taskStrike",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskBridge",
+				name = "taskBridge",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskStrikeListen",
+				name = "taskStrikeListen",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							params = {
+								count = 10,
+								total = 10,
+								
+							},
+							type = "COUNT",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskOgden",
+				name = "taskOgden",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskMoneyOffer1",
+				name = "taskMoneyOffer1",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskConnectOgden",
+				name = "taskConnectOgden",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "BOOL",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskConnect",
+				name = "taskConnect",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskMoneyOffer2",
+				name = "taskMoneyOffer2",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskDamagedBridge",
+				name = "taskDamagedBridge",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = false,
+							params = {
+								count = 4,
+								total = 6,
+								
+							},
+							type = "COUNT",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = true,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						
+					},
+					userState = {
+						progress = {
+							count = 4,
+							
+						},
+						trains = {
+							[12309] = 4,
+							
+						},
+						
+					},
+					
+				},
+				id = "taskSendTrain",
+				name = "taskSendTrain",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskMoneyOffer3",
+				name = "taskMoneyOffer3",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskMoneyOffer4",
+				name = "taskMoneyOffer4",
+				
+			}, {
+				data = {
+					progress = {
+						indicator = {
+							completed = true,
+							type = "NONE",
+							
+						},
+						medals = {
+							
+						},
+						optionsAvailable = false,
+						subIndicators = {
+							
+						},
+						unavailableOptions = {
+							
+						},
+						
+					},
+					result = {
+						optionValue = "1",
+						
+					},
+					userState = {
+						
+					},
+					
+				},
+				id = "taskMoneyOffer5",
+				name = "taskMoneyOffer5",
+				
+			}, 
+		}, 
+	},
+	
+}
+end
